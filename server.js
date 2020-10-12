2ee7c3e45e04ccde1519f58579c7ffe176981da9
hasana <hasan.ahmed@thetrainline.com>
2020-10-12 20:08:34 +0530

    fix heroku deployment

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(3000,() => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000,() => console.log('Listening on port 3000'));
