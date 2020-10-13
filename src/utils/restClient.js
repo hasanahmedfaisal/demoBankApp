ea9bf9dbe9b30e6c7b81edd5f605abd9f1a4ef64
hasana <hasan.ahmed@thetrainline.com>
2020-10-13 20:00:02 +0530

    update api url

--- src/utils/restClient.js
+++ src/utils/restClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getApiUrl = (url) => {
-    return `http://localhost:3001${url}`
+    return `https://bankmanagementsystemapp.herokuapp.com${url}`
 }
 
 const get = async (url, queryParams) => {
